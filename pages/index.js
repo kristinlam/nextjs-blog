cc6fd26303ebb4a3c309de19abfc69306e31e229
Kristin Lam <klam623@gmail.com>
2023-02-28 02:30:22 -0500

    Fetch data using static generation

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,18 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/Layout';
 import utilStyles from '../styles/Utils.module.css';
 import Link from 'next/link';
+import { getSortedPostsData } from '../lib/posts';
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
@@ -17,6 +27,18 @@ export default function Home() {
           and The Bowery Mission.
         </p>
         <Link href="/posts/first-post">Check out my first post</Link>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title, author }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title} by {author}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
